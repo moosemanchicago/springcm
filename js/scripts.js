ce0c6f683938838883bd374ca5933f75537f9638
Mustafa Ishaq <moose@007-laptop.local>
2015-06-22 21:09:42 -0500

    converted to all angular, no manual ajax and no list.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,99 +1,68 @@
 //"iffy" - IIFE
-	// (function() {
+// (function() {
 	
 	var initiateAngular = function() {
+
     	angular.module("DocumentsApp", [])
-        	.controller("MainController", ["$scope", MainController]);
+        		.controller("MainController", ["$scope", "$http", MainController]);
 	
-		function MainController($scope) {
-        	$scope.mustafa = "Mustafa R. Ishaq";
-        
+		function MainController($scope, $http) {
+
+			// developer information object
+			var developer = {
+				firstName: "Mustafa",
+				lastName: "Ishaq",
+				location: "Schaumburg",
+				relocation: "Willing to relocate.",
+				intro: "I am super excited to be on your team!"
+			}
+
+			// bring developer into scope
+			$scope.developer = developer;
+
+
+
+			// error messages
+			var onError1 = function(){
+				$scope.error = "Something went wrong with the springCMDataPromise."
+			}
+
+			var onError2 = function(){
+				$scope.error = "Something went wrong with the actionPromise."
+			}
+
+
+
+			// get the user documents from the json file springCMData.json
+        	var springCMDataPromise = $http.get('springCMData.json');
+        	var gatherDataComplete = function(response){
+        		//bring user documents into scope after promise is completed
+				$scope.springCMData = response.data.userDocuments;
+				
+				console.log("User Documents:");
+				console.log($scope.springCMData);
+			}
+			springCMDataPromise.then(gatherDataComplete, onError1);
+
+
+
+
+			// get the user actions from the json file actionData.json
+			var actionPromise = $http.get('actionData.json');
+			var gatherActionsComplete = function(response){
+				//bring user actions into scope after promise is completed
+				$scope.userActions = response.data.actions;
+
+				console.log("User Actions:");
+				console.log($scope.userActions);
+			}
+			actionPromise.then(gatherActionsComplete, onError2);
+
+
     	};
 	}
 
-
-    
-
-
-	// bring in the clients assets to the view
-	var fetchUserDocuments  = function(){
-		
-		// create new XHR object to have fun with
-		var documentsXHR = new XMLHttpRequest();
-
-		// when the onload event is fired with "OK 200", do work with the the JSON object that was received
-		documentsXHR.onload = function(){
-			if( documentsXHR.status == 200){
-				//access data from the JSON object property "userDocuments"
-				var gridData = JSON.parse(documentsXHR.responseText).userDocuments;
-				//console.log(gridData);
-
-				var headerTitles = {
-		    		item: '<article><span class="type"></span><span class="name"></span><span class="description"></span><span 	class="modifiedDate"></span	></article>'
-				};
-		
-				var springCMData = new List('scrollable-grid', headerTitles, gridData);
-		
-				console.log("AJAX:   springCMData Loaded successfully from .json file");
-		
-			} else {
-				console.log("AJAX:   failed to load grid data from .json file");
-			}
-		}
-		
-		// prepare the request
-		documentsXHR.open('GET','springCMData.json',true);
-
-		// have fun
-		documentsXHR.send();
-	
-	};
-	
-	
-	// bring in the actions to the view; less comments in this section because its practically
-	// the same thing as _fetchUserDocuments();
-	var fetchUserActions  = function(){
-	
-		var actionsXHR = new XMLHttpRequest();
-		actionsXHR.onload = function(){
-			if( actionsXHR.status == 200){
-
-				var actionData = JSON.parse(actionsXHR.responseText).actions;
-
-				var actionControls = '';
-
-				for(var x = 0; x < 3; x++){
-					actionControls += '<div class="action_button"><a href="javascript:void(0)"><img src="img/' + actionData[x].imageName + '.png" title="' + actionData[x].name + '"/></a></div>';
-				}
-
-				document.getElementById('action-drawer').innerHTML = actionControls;
-
-
-				// var actionTitles = {
-		  //   		item: '<article><span class="action_button name"></span><span class="action_button imageName"></span></article>'
-				// };
-
-				// var actionButtons = new List('action-drawer', actionTitles, actionData);
-
-
-				console.log("AJAX:   actionData Loaded successfully from .json file");
-			} else {
-				console.log("AJAX:   failed to load action data from .json file");
-			}
-		}
-	
-		actionsXHR.open('GET','actionData.json',true);
-		actionsXHR.send();
-	
-	};
-	
-
-
 	// functions!
 	initiateAngular();
-	fetchUserDocuments();
-	fetchUserActions();
-	
-	
-	
-	// }());
+
+// }());
